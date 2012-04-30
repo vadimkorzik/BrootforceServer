3cf3830f337d1666fa2582e16ca2ca3ea62ce015
vadimkorzik <k.vadim.exe@gmail.com>
2012-04-30 20:19:04 +0300

    blah - blah

--- src/com/vadim/Brootforce/Main.java
+++ src/com/vadim/Brootforce/Main.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 /**
  * User: Vadim | Date: 14.04.12 | Time: 20:51
  */
+
 public class Main {
     public static void main(String[] args) throws IOException {
         NetworkManager nm = new NetworkManager();
