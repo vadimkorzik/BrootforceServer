059f7a88b2e84b5cfca37a2bfe9c1809b97f401b
vadimkorzik <k.vadim.exe@gmail.com>
2012-05-02 22:46:27 +0300

    extend Commands

--- src/com/vadim/Bruteforce/network/Commands.java
+++ src/com/vadim/Bruteforce/network/Commands.java
@@ -1,5 +1,7 @@
 package com.vadim.Bruteforce.network;
 
+import java.nio.ByteBuffer;
+
 /**
  * User: Vadim | Date: 02.05.12 | Time: 22:20
  */
@@ -17,5 +19,25 @@ public class Commands {
     // Codes: server -> client
     public static final byte MESSAGE_OF_STOP = 32;
 
-
+    /**
+     * Copy String to ByteBuffer from 1 index <b>(not 0)</b> .
+     * Automatic allocate memory
+     *
+     * @param str    String to send
+     * @param buffer buffer to send
+     */
+    public static void stringToBuffer(String str, ByteBuffer buffer) {
+        byte[] bytes = str.getBytes();
+        int i = 1;
+        begin:
+        for (byte b : bytes) {
+            try {
+                buffer.put(i, b);
+                i++;
+            } catch (ArrayIndexOutOfBoundsException e) {
+                buffer = ByteBuffer.allocate(bytes.length + 2);
+                Commands.stringToBuffer(str, buffer);
+            }
+        }
+    }
 }
